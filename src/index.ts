8d3f9573d42f9d1bea280ecfe81b05b918823f8f
Stevan Goode <steve@madhouseminers.com>
2020-08-26 18:56:17 +0100

    Removed subscription

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,6 @@
 import fetch from "node-fetch";
-import gql from "graphql-tag";
-import { apolloClient } from "./graphql";
-import { sendWhitelist } from "./commands/whitelistFetch";
 
-const query = gql`
-  subscription {
-    whitelistUpdated {
-      id
-      displayName
-      dob
-      whereHeard
-      moddedExperience
-      knownMembers
-      interestedServers
-      aboutUser
-      status
-    }
-  }
-`;
-
-let observe = apolloClient.subscribe({
-  query,
-});
-observe.subscribe(async (result) => {
-  const whitelist = result.data.whitelistUpdated;
-  switch (whitelist.status) {
-    case "SUBMITTED":
-      await sendWhitelist(whitelist);
-      break;
-    case "ACCEPTED":
-      await whitelistUser(whitelist.displayName);
-      break;
-  }
-});
-
-async function whitelistUser(displayName) {
+export async function whitelistUser(displayName) {
   const serverResults = await fetch(
     "https://panel.madhouseminers.com/api/application/servers",
     {
